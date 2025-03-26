54329c2f886c236851f4f5952b00b86101b3a4ca
Leandro <leandro_p@hotmail.it>
2025-03-26 16:55:31 +0100

    ho diviso il sito in parti di viste blade

---
+++ resources/views/partials/services.blade.php
@@ -0,0 +1,55 @@
+    <!-- Services Section -->
+    <section id="services" class="services section light-background">
+
+      <!-- Section Title -->
+      <div class="container section-title" data-aos="fade-up">
+        <h2>I nostri principali Servizi</h2>
+        
+      </div><!-- End Section Title -->
+
+      <div class="container">
+
+        <div class="row gy-4">
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+            <div class="service-item  position-relative">
+              <div class="icon">
+                <i class="fa-solid fa-mountain-city"></i>
+              </div>
+              <h3>Nuvole di punti</h3>
+              <p>Acquisizione ad alta precisione di ambienƟ e struƩure tramite laser scanner 3D, generando modelli
+                deƩagliaƟ per analisi, rilievi e progeƩazione.</p>
+              <a href="#" class="readmore stretched-link">Read more <i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div><!-- End Service Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+            <div class="service-item position-relative">
+              <div class="icon">
+                <i class="fa-solid fa-arrow-up-from-ground-water"></i>
+              </div>
+              <h3>Scan to BIM</h3>
+              <p>Conversione delle nuvole di punƟ in modelli BIM intelligenƟ, ideali per la progeƩazione civile e
+                industriale, la ristruƩurazione, la manutenzione di infrastruƩure, edifici e impianƟ.</p>
+              <a href="#" class="readmore stretched-link">Read more <i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div><!-- End Service Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
+            <div class="service-item position-relative">
+              <div class="icon">
+                <i class="fa-solid fa-compass-drafting"></i>
+              </div>
+              <h3>PROGETTAZIONE STRUTTURALE</h3>
+              <p>Analisi e calcolo di struƩure nuove ed esistenƟ, con soluzioni di rinforzo, consolidamento e
+                oƫmizzazione basate su normaƟve e tecnologie avanzate.</p>
+              <a href="#" class="readmore stretched-link">Read more <i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div><!-- End Service Item -->
+
+
+        </div>
+
+      </div>
+
+    </section><!-- /Services Section -->
